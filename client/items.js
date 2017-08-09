b81c03d88eb3e84d7cd6b34f5ab50d59d08b6a72
nickgreenquist <nickgreenquist@gmail.com>
2017-08-09 19:00:32 -0400

    migrated item stats for armor and weapons out to another script

---
+++ client/items.js
@@ -0,0 +1,95 @@
+let classes = [
+    {
+        "name": "WARRIOR",
+        "health": 0,
+        "speed": 0,
+        "intelligence": -10,
+    },
+    {
+        "name": "MUSTACHE",
+        "health": 15,
+        "speed": -10,
+        "intelligence": 25,
+    },
+    {
+        "name": "MAN BUN",
+        "health": -10,
+        "speed": 25,
+        "intelligence": -50,
+    },
+    {
+        "name": "NINJA",
+        "health": -50,
+        "speed": 50,
+        "intelligence": 0,
+    },
+    {
+        "name": "GREEN BERET",
+        "health": 25,
+        "speed": 25,
+        "intelligence": 25,
+    },
+];  
+
+let weapons = [
+    {
+        "name": "SWORD",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "POLEARM",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "STAFF",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "CLEAVER",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "SHAMAN BLADE",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "HERO SWORD",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "WIZARD STAFF",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "SERPENT STAFF",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "MASTER SWORD",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+    {
+        "name": "TRI SWORD",
+        "attack": 0,
+        "speed": 0,
+        "intelligence": 0,
+    },
+]; 
